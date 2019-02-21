82fc462a7acb7460e4bd654b343b9d3e93d20bc2
dehezhang2 <dehezhang2@gmail.com>
2019-02-22 01:41:43 +0800

    vimsetings

---
+++ vim/test.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+    cout << "Hello wold!\n";
+    for(int i=0;i<100;i++){
+        cout << "ahha" << endl;
+        if(i){
+            cout << i << endl;
+        }
+    }
+    return 0;
+}
